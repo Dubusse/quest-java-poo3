8f59ecd4b6a64ba579dc88e1d7fde654e01a9019
Arthur Dubusse <arthur.dubusse@gmail.com>
2019-10-03 16:39:20 +0200

    fait

--- Eagle.java
+++ Eagle.java
@@ -1,24 +1,41 @@
-public class Eagle extends Bird {
+public class Eagle extends Bird implements Fly {
 
-    private boolean flying;
-    private int altitude;
+	private boolean flying;
+	private int altitude;
 
-    public Eagle(String name) {
-        super(name);
-        this.flying = false;
-        this.altitude = 0;
-    }
+	public Eagle(String name) {
+	super(name);
+	this.flying = false;
+	this.altitude = 0;
+	}
+	public int getAltitude() {
+	return altitude;
+	}
+	public boolean isFlying() {
+	return flying;
+	}
+	@Override
+	public String sing() {
+	return "Screech!";
+	}
+	@Override
+	public int ascend(int meters) {
+	// TODO Auto-generated method stub
+	return 0;
+	}
+	@Override
+	public int descend(int meters) {
+	// TODO Auto-generated method stub
+	return 0;
+	}
+	@Override
+	public void land() {
+	// TODO Auto-generated method stub
 
-    public int getAltitude() {
-        return altitude;
-    }
+	}
+	@Override
+	public void takeOff() {
+	// TODO Auto-generated method stub
 
-    public boolean isFlying() {
-        return flying;
-    }
-
-    @Override
-    public String sing() {
-        return "Screech!";
-    }
+	}
 }
